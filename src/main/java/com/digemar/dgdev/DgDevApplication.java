2b7156c7f4021455dc7d7ab5ef13c435d170ced9
gvilca29 <george.vilca@tecsup.edu.pe>
2024-07-16 19:32:12 +0000

    Inicio Proyecto

---
+++ src/main/java/com/digemar/dgdev/DgDevApplication.java
@@ -0,0 +1,13 @@
+package com.digemar.dgdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgDevApplication.class, args);
+	}
+
+}
